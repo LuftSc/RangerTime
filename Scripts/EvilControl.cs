2ff6b6ea432808306b2e32e8e38a0f4603c21fee
AmLear <artem.zverev.03@bk.ru>
2023-12-08 16:13:08 +0500

    Добавлена логика врага
    Враг может ходить в своей местности

--- Scripts/EvilControl.cs
+++ Scripts/EvilControl.cs
@@ -3,22 +3,41 @@ using System;
 
 public partial class EvilControl : CharacterBody2D
 {
-    public const float Speed = 400.0f;
+    private Vector2 velocity;
+    private float speed = 100; 
 
     private AnimatedSprite2D _evilSprite;
+
+    private float distanceMoved = 0; 
+    private float maxDistance = 50000; 
+
+    private bool movingRight = true; 
+
     public override void _Ready()
-	{
+    {
         _evilSprite = GetNode<AnimatedSprite2D>("AnimationSprite");
     }
 
     public override void _PhysicsProcess(double delta)
-    {
-        _UpdateSpriteRenderer();
+    {        
+        if (distanceMoved >= maxDistance)
+        {            
+            movingRight = !movingRight;
+            distanceMoved = 0;
+        }
 
+        float direction = movingRight ? -100 : 100;
+        velocity.X = speed * direction;
+
+        _UpdateSpriteRenderer();
+        Velocity = velocity * (float)delta;
+        MoveAndSlide();
+
+        distanceMoved += Mathf.Abs(velocity.X * (float)delta);
     }
 
     private void _UpdateSpriteRenderer()
-    {        
+    {
         _evilSprite.Play("evilSprite");
     }
 }
